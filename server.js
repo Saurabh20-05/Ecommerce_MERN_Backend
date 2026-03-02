56cdfb5c5981d6963c82672edad05d72fc5876b2
Saurabh20-05 <saurabh200805@gmail.com>
2026-03-02 05:34:30 +0530

    removed app.options fix

--- server.js
+++ server.js
@@ -22,14 +22,11 @@ app.use(
       "http://localhost:3000",
       "https://ecommerce-mern-frontend-eta.vercel.app"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
 
 // VERY IMPORTANT
-app.options("*", cors());
 
 app.use(
   "/uploads",
